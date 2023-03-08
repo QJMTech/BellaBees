d62b62904ad4b3303f62cd11c9d78d4504521f45
QMels <qjm7@nau.edu>
2023-03-07 23:33:44 -0700

    New build

---
+++ dist/bb-store/assets/stringliteral.constants.ts
@@ -0,0 +1,12 @@
+export const MESSAGES = {
+  alerts: {
+    addToCartSuccess: 'Item added to cart! :)',
+    addToCartWarning: 'Item already in cart. :(',
+    cartClearedSuccess: 'Your cart has been cleared! :)',
+    removeFromCartSuccess: 'Item removed from cart! :)'
+    },
+  notification: {
+    info: 'For your information...',
+  },
+  header: 'From My Closet To Yours'
+}
